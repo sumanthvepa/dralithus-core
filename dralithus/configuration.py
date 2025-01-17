f6027e98a95037559aa9472eeeee60dc414c27ca
Sumanth Vepa <svepa@milestone42.com>
2025-01-17 17:23:37 +0530

    issue002-release0-print-command-line-parameters: Skeleton API definition for the process_command_line function
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified].idea/inspectionProfiles/Project_Default.xml:
      Ignore PEP8 E111 and E114 errors
    [modified].idea/misc.xml:
      Set the project python SDK correctly
    [new]dralithus/configuration.py:
      Configuration utilities for the dralithus application
    [modified]drl:
      Add call to process_command_line function and print its output
    
    DESCRIPTION
    Skeleton API definition for the process_command_line function

---
+++ dralithus/configuration.py
@@ -0,0 +1,89 @@
+"""
+  configuration.py: Command line processing for dralithus
+"""
+from typing import TypedDict
+
+
+class CommandLineError(RuntimeError):
+  """
+    CommandLineError: An error occurred while processing the command line
+  """
+
+  def __init__(self, message: str) -> None:
+    """
+      Initialize the CommandLineError
+      :param message: The error message
+    """
+    super().__init__(message)
+
+
+class Operation(TypedDict):
+  """
+    The operation that drl is expected to perform
+
+    The operation that drl is expected to perform is described by this
+    dictionary. The dictionary contains the following keys:
+    - command: str: The command to be executed
+    - about: None | str: If the command is help, then about will contain the
+        command to get help on. Or it can be None if global help (about the
+        whole program) is requested. If the command is not help,  then about
+        will always be None.
+    - applications: None | list[str]: The applications to be deployed. The value
+        will be None if the command does not take a list of applications.
+    - environment: None | list[str]: The environments to deploy the application
+        to. The value will be None if the command does not take a list of
+        environments.
+    - verbosity: int:  The verbosity level. 0 is the default.
+  """
+  command: str
+  about: str | None
+  applications: list[str] | None
+  environments: list[str] | None
+  verbosity: int
+
+
+# pylint: disable=unused-argument
+# noinspection PyUnusedLocal
+def process_command_line(args: list[str]) -> Operation:
+  """
+    Process command line arguments
+
+    The function takes a list of command line arguments and returns a typed
+    dictionary (Operation) that describes the operation that drl is expected to
+    perform.
+
+
+    If an error is encountered while processing the command line,
+    a CommandLineError exception is raised.
+
+    We pass the command line arguments as a parameter to the function rather
+    than using sys.argv directly. This allows us to test the function with
+    different command line arguments without having to modify the actual command
+    line.
+
+    TODO: args is not used, but will be, when the function is implemented.
+
+    :param list[str] args: The command line arguments (typically sys.argv)
+
+    :return: A dictionary containing details about the command to be executed
+    The dictionary will contain the following keys:
+    - command: str: The command to be executed
+    - about: None | str: If the command is help, then about will contain the
+        command to get help on. Or it can be None if global help (about the
+        whole program) is requested. If the command is not help,  then about will
+        always be None.
+    - applications: None | list[str]: The applications to be deployed. The value
+        will be None if the command is does not take a list of applications.
+    - environment: None | list[str]: The environments to deploy the application
+        to. The value will be None if the command does not take a list of
+        environments.
+    - verbosity: int:  The verbosity level. 0 is the default.
+  """
+  # TODO: Implement this.
+  return {
+    'command': 'help',
+    'about': None,
+    'applications': None,
+    'environments': None,
+    'verbosity': 0
+  }
