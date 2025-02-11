d77b9d7b84b713c6aed995374ceceb8433bacf5e
Sumanth Vepa <svepa@milestone42.com>
2025-02-11 16:24:02 +0530

    issue002-release0-print-command-line-parameters: Implement deploy_valid_test_cases, that generates a list of test cases where the deploy command is specified correctly.
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/test/configuration/process_command_line/test_deploy.py:
    Implement deploy_valid_test_cases, that generates a list of test cases
    where the deploy command is specified correctly.
    
    DESCRIPTION
    Implement deploy_valid_test_cases, that generates a list of test cases
    where the deploy command is specified correctly.

--- dralithus/test/configuration/process_command_line/test_deploy.py
+++ dralithus/test/configuration/process_command_line/test_deploy.py
@@ -6,33 +6,54 @@
 """
 from parameterized import parameterized
 
+from dralithus.configuration import Operation
+
 from dralithus.test.configuration.process_command_line import (
-  Args,
   TestCaseData,
   CommandLineTestCase,
+  make_args_list,
+  make_test_cases,
   all_test_cases,
   print_cases)
 
-from dralithus.configuration import CommandLineError
 
+def deploy_valid_test_cases() -> list[tuple[TestCaseData]]:
+  """
+    Test cases representing invocation of drl with the 'deploy' command and
+    various ways to CORRECTLY specify the environment and applications.
 
-def no_parameters_test_cases() -> list[tuple[TestCaseData]]:
-  """
-    Test cases representing invocation of drl with the 'deploy' command
-    and no parameters.
-  """
-  args = Args(program='drl', global_options=[], command='deploy', command_options=[], parameters=[])
-  error =  {'error_type': CommandLineError, 'verbosity': 0}
-  return [(TestCaseData(args=args, expected=None, error=error),)]
+    :return: A list of test cases where the 'deploy' command is specified and
+    the environment and applications are specified correctly.
+"""
+  args_list = make_args_list(
+    program='drl',
+    global_options_list=[[]],
+    command_list=['deploy'],
+    command_options_list=[
+      ['--environment=local'],
+      ['--environment', 'local'],
+      ['-elocal'],
+      ['-e', 'local']],
+    parameters_list=[[]])
+  expected: Operation = {
+    'command': 'deploy',
+    'about': None,
+    'environments': ['local'],
+    'applications': ['sample'],
+    'verbosity': 0}
+  return make_test_cases(args_list, expected, None)
+
 
 def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   """
     Test cases representing invocation of drl with the 'deploy' command
     and various ways to specify the environment and applications.
+
+  :return: A list of test cases where the 'deploy' command is specified.
   """
   # TODO: Complete this implementation
   cases: list[tuple[TestCaseData]] = []
-  cases += no_parameters_test_cases()
+  cases += deploy_valid_test_cases()
   return cases
 
 
@@ -48,4 +69,5 @@ class TestDeploy(CommandLineTestCase):
 
 
 if __name__ == '__main__':
+  # print_cases(deploy_test_cases())
   print_cases(all_test_cases(deploy_base_test_cases()))
