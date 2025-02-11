3dd1fc4d683acca084d67e9c35e17c41abd98054
Sumanth Vepa <svepa@milestone42.com>
2025-02-11 18:47:35 +0530

    issue002-release0-print-command-line-parameters: Add tests to check that the deploy command without an application name results in an error
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/test/configuration/process_command_line/test_deploy.py:
      Add tests to check that the deploy command without an application name
      results in an error
    
    DESCRIPTION
    Add tests to check that the deploy command without an application name
    results in an error.

--- dralithus/test/configuration/process_command_line/test_deploy.py
+++ dralithus/test/configuration/process_command_line/test_deploy.py
@@ -6,9 +6,10 @@
 """
 from parameterized import parameterized
 
-from dralithus.configuration import Operation
+from dralithus.configuration import Operation, CommandLineError
 
 from dralithus.test.configuration.process_command_line import (
+  ErrorDict,
   TestCaseData,
   CommandLineTestCase,
   make_args_list,
@@ -46,6 +47,26 @@ def deploy_valid_test_cases() -> list[tuple[TestCaseData]]:
   return make_test_cases(args_list, expected, None)
 
 
+def deploy_missing_application_test_cases() -> list[tuple[TestCaseData]]:
+  """
+    Test cases where the application name is missing
+  """
+  args_list = make_args_list(
+    program='drl',
+    global_options_list=[[]],
+    command_list=['deploy'],
+    command_options_list=[
+      ['--environment=local'],
+      ['--environment', 'local'],
+      ['--env=local'],
+      ['--env', 'local'],
+      ['-elocal'],
+      ['-e', 'local']],
+    parameters_list=[[]])
+  error: ErrorDict = {'error_type': CommandLineError, 'verbosity': 0 }
+  return make_test_cases(args_list, None, error)
+
+
 def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   """
     Test cases representing invocation of drl with the 'deploy' command
@@ -56,6 +77,7 @@ def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   # TODO: Complete this implementation
   cases: list[tuple[TestCaseData]] = []
   cases += deploy_valid_test_cases()
+  cases += deploy_missing_application_test_cases()
   return cases
 
 
@@ -71,5 +93,5 @@ class TestDeploy(CommandLineTestCase):
 
 
 if __name__ == '__main__':
-  # print_cases(deploy_test_cases())
+  # print_cases(deploy_base_test_cases())
   print_cases(all_test_cases(deploy_base_test_cases()))
