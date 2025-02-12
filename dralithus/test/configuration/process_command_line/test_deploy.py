9608ae40009f921df070062a01e974fc69485440
Sumanth Vepa <svepa@milestone42.com>
2025-02-12 08:57:27 +0530

    issue002-release0-print-command-line-parameters: Add tests to check that the deploy command without an environment specified results in an error
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/test/configuration/process_command_line/test_deploy.py:
      Define deploy_missing_environment_test_cases to check that the deploy command
      without an environment specified results in an error.
    
    DESCRIPTION
    Add tests to check that the deploy command without an environment
    specified results in an error.

--- dralithus/test/configuration/process_command_line/test_deploy.py
+++ dralithus/test/configuration/process_command_line/test_deploy.py
@@ -67,6 +67,20 @@ def deploy_missing_application_test_cases() -> list[tuple[TestCaseData]]:
   return make_test_cases(args_list, None, error)
 
 
+def deploy_missing_environment_test_cases() -> list[tuple[TestCaseData]]:
+  """
+    Test cases where the environment name is missing
+  """
+  args_list = make_args_list(
+    program='drl',
+    global_options_list=[[]],
+    command_list=['deploy'],
+    command_options_list=[[]],
+    parameters_list=[['sample']])
+  error: ErrorDict = {'error_type': CommandLineError, 'verbosity': 0 }
+  return make_test_cases(args_list, None, error)
+
+
 def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   """
     Test cases representing invocation of drl with the 'deploy' command
@@ -78,6 +92,7 @@ def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   cases: list[tuple[TestCaseData]] = []
   cases += deploy_valid_test_cases()
   cases += deploy_missing_application_test_cases()
+  cases += deploy_missing_environment_test_cases()
   return cases
 
 
