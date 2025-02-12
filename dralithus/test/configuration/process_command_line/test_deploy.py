54175c6aeb3be39c6cf237c2e48f3dcd86ca109e
Sumanth Vepa <svepa@milestone42.com>
2025-02-12 14:05:18 +0530

    issue002-release0-print-command-line-parameters: Add tests to check that the deploy command with with multiple valid environments creates the correct operation.
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/test/configuration/process_command_line/test_deploy.py:
      Define deploy_multi_environment_test_cases to check that the
      deploy command with with multiple valid environments creates the
      correct operation.
    
    DESCRIPTION
    Add tests to check that the deploy command with with multiple valid
    environments creates the correct operation.

--- dralithus/test/configuration/process_command_line/test_deploy.py
+++ dralithus/test/configuration/process_command_line/test_deploy.py
@@ -47,6 +47,41 @@ def deploy_valid_test_cases() -> list[tuple[TestCaseData]]:
   return make_test_cases(args_list, expected, None)
 
 
+def deploy_valid_multi_environment_test_cases() -> list[tuple[TestCaseData]]:
+  """
+    Test cases representing invocation of drl with the 'deploy' command and
+    various ways to CORRECTLY specify multiple environments and applications.
+
+    :return: A list of test cases where the 'deploy' command is specified and
+    the environment and applications are specified correctly.
+  """
+  args_list = make_args_list(
+    program='drl',
+    global_options_list=[[]],
+    command_list=['deploy'],
+    command_options_list=[
+      ['--environment=local', '--environment=dev'],
+      ['--environment', 'local', '--environment', 'dev'],
+      ['--env=local', '--env=dev'],
+      ['--env', 'local', '--env', 'dev'],
+      ['-elocal', '-edev'],
+      ['-e', 'local', '-e', 'dev'],
+      ['--environment=local,dev'],
+      ['--environment', 'local,dev'],
+      ['--env=local,dev'],
+      ['--env', 'local,dev'],
+      ['-elocal,dev'],
+      ['-e', 'local,dev']],
+    parameters_list=[['sample']])
+  expected: Operation = {
+    'command': 'deploy',
+    'about': None,
+    'environments': ['local', 'dev'],
+    'applications': ['sample'],
+    'verbosity': 0}
+  return make_test_cases(args_list, expected, None)
+
+
 def deploy_missing_application_test_cases() -> list[tuple[TestCaseData]]:
   """
     Test cases where the application name is missing
@@ -165,6 +200,7 @@ def deploy_base_test_cases() -> list[tuple[TestCaseData]]:
   # TODO: Complete this implementation
   cases: list[tuple[TestCaseData]] = []
   cases += deploy_valid_test_cases()
+  cases += deploy_valid_multi_environment_test_cases()
   cases += deploy_missing_application_test_cases()
   cases += deploy_missing_environment_test_cases()
   cases += deploy_missing_environment_and_application_test_cases()
