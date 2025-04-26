06679553fdf4b622a37c3ebe2212cf0903f2f8a6
Sumanth Vepa <svepa@milestone42.com>
2025-04-26 09:55:25 +0530

    issue002-release0-print-command-line-parameters: Define class Command
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [new]]dralithus/errors.py:
      Define class Command
    
    DESCRIPTION
    Define class Command
    
    This is the base class for all Commands in dralithus.
    All subclasses must implement the execute method.

---
+++ dralithus/command.py
@@ -0,0 +1,67 @@
+"""
+  command.py: Define the Command abstract base class.
+"""
+# -------------------------------------------------------------------
+# command.py: Define the Command abstract base class.
+#
+# Copyright (C) 2023-25 Sumanth Vepa.
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License a
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------
+from abc import ABC, abstractmethod
+
+
+class Command(ABC):
+  """
+    A dralithus command that can be executed
+  """
+  def __init__(self, name: str, verbosity: int) -> None:
+    """
+      Initialize the command with a verbosity level.
+
+      :param verbosity: The verbosity level of the command
+    """
+    self._name = name
+    self._verbosity = verbosity
+
+  @property
+  def name(self) -> str:
+    """
+      The name of the command.
+
+      :return: The name of the command
+    """
+    return self._name
+
+  @property
+  def verbosity(self) -> int:
+    """
+      The verbosity level of the command.
+
+      :return: The verbosity level of the command
+    """
+    return self._verbosity
+
+  @abstractmethod
+  def execute(self) -> int:
+    """
+      Execute the command.
+
+      Any class derived from this class must implement this method
+      to execute the actual command.
+
+      :return: The program exit code
+    """
+    raise NotImplementedError("execute() must be implemented in derived class")
