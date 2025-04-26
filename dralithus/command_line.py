2509ab2f7a49068b241e20fc5081096b823c68cf
Sumanth Vepa <svepa@milestone42.com>
2025-04-27 01:29:07 +0530

    issue002-release0-print-command-line-parameters: Move definition of class CommandLineError from command_line.py to errors.py
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/command_line.py:
      Move definition of class CommandLineError to errors.py
    [modified]dralithus/errors.py:
      Move definition of class CommandLineError from command_line.py
      to this file
    
    DESCRIPTION
    Move definition of class CommandLineError from command_line.py
    to errors.py.
    
    This is done because the class CommandLine in file command_line.py (to
    be defined in a future commit) as well as class CommandLineParser in
    file command_line_parser.py (also to be defined in a future commit,)
    both use the class CommandLineError.  However, the class CommandLine
    will use class CommandLineParser. This will result in a circular
    dependency.  To avoid this, the class CommandLineError is moved to
    file errors.py.  errors.py is also a more appropriate place for
    exception classes.

--- dralithus/command_line.py
+++ dralithus/command_line.py
@@ -1,26 +1,3 @@
 """
-  command_line.py: Define class CommandLine and CommandLineError
+  command_line.py: Define class CommandLine.
 """
-
-from dralithus.errors import DralithusError, ExitCode
-
-
-class CommandLineError(DralithusError):
-  """
-    Exception raised for invalid command line arguments passed
-    by the user.
-
-    Unlike other exceptions, this exception is caught by code
-    that parses the command line and converted into a help command
-    that is executed. The help command when executed will print
-    the error, and then provide an appropriate help message.
-  """
-  def __init__(self, message: str) -> None:
-    """
-      Initialize the InvalidCommandLineError with a message.
-
-      The exit code is set to ExitCode.INVALID_COMMAND_LINE.
-
-      :param message: The error message
-    """
-    super().__init__(message, exit_code=ExitCode.INVALID_COMMAND_LINE)
--- dralithus/errors.py
+++ dralithus/errors.py
@@ -2,7 +2,7 @@
   errors.py:  Define the DralithusError class
 """
 # -------------------------------------------------------------------
-# errors.py:  Define the DralithusError class
+# errors.py:  Define the DralithusError class and subclasses.
 #
 # Copyright (C) 2023-25 Sumanth Vepa.
 #
@@ -66,3 +66,24 @@ class DralithusError(RuntimeError):
       :return: The exit code of the error
     """
     return self._exit_code
+
+
+class CommandLineError(DralithusError):
+  """
+    Exception raised for invalid command line arguments passed
+    by the user.
+
+    Unlike other exceptions, this exception is caught by code
+    that parses the command line and converted into a help command
+    that is executed. The help command when executed will print
+    the error, and then provide an appropriate help message.
+  """
+  def __init__(self, message: str) -> None:
+    """
+      Initialize the InvalidCommandLineError with a message.
+
+      The exit code is set to ExitCode.INVALID_COMMAND_LINE.
+
+      :param message: The error message
+    """
+    super().__init__(message, exit_code=ExitCode.INVALID_COMMAND_LINE)
