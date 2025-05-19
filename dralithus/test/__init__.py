9a2757f519169c39b950afe8918fc489a2098ed9
Sumanth Vepa <svepa@milestone42.com>
2025-05-19 07:47:24 +0530

    issue002-release0-print-command-line-parameters: Define a mixin class, CaseExecutor that allows for execution dralithus cli test cases
    FEATURE(s):
    issue002-release0-print-command-line-parameters:
      Parse the command line and print the environment name and the
      application name
    
    FILE(s)
    [modified]dralithus/test/__init__.py
      Define class CaseData that encapsulates the expected input and
      output of from invoking dralithus
      Define a protocol class RequiresAsserts that specifes the signatures
      of methods that any class that needs to use the CaseExecutor mixin
      must support
      Define mixin class CaseExecutor that implements the execute method
      to execute a CaseData test case.
    
    DESCRIPTION
    Define a mixin class, CaseExecutor that allows for execution dralithus
    cli test cases.

--- dralithus/test/__init__.py
+++ dralithus/test/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-dralithus/test/__init__.py: Unit tests and helper classes/functions
-to test the dralithus module.
+dralithus/test/__init__.py: Helper classes and functions for unit tests
 """
 # -------------------------------------------------------------------
-# dralithus/test/__init__.py: Unit tests and helper classes/functions
-# to test the dralithus module.
+# dralithus/test/__init__.py: Helper classes and functions for unit
+# tests
 #
 # Copyright 2023-25. Sumanth Vepa. svepa@milestone42.com
 #
@@ -24,3 +23,107 @@ to test the dralithus module.
 # You should have received a copy of the GNU General Public License
 # along with dralithus-core. If not, see <https://www.gnu.org/licenses/>.
 # -------------------------------------------------------------------
+from typing import Callable, Protocol
+
+
+class CaseData:
+  """
+    A test case for dralithus
+  """
+  def __init__(self,
+      args: list[str],
+      expected: tuple[dict[str, None | bool | int | str | set[str]], set[str]] | None,
+      error: type[Exception] | None):
+    """
+      Initialize the test case.
+
+      :param args: The input to the test case
+      :param expected: The expected output of the test case
+      :param error: The expected error of the test case
+    """
+    assert ((expected is not None) and (error is None)) \
+           or ((expected is None) and (error is not None)), \
+      "If expected is set, then error must be none, and vice versa."
+    self._args = args
+    self._expected = expected
+    self._error = error
+
+  @property
+  def args(self) -> list[str]:
+    """
+      Get the input of the test case.
+
+      :return: The input of the test case
+    """
+    return self._args
+
+  @property
+  def expected(self) -> tuple[dict[str, None | bool | int | str | set[str]], set[str]] | None:
+    """
+      Get the expected output of the test case.
+
+      :return: The expected output of the test case
+    """
+    return self._expected
+
+  @property
+  def error(self) -> type[Exception] | None:
+    """
+      Get the expected error of the test case.
+
+      :return: The expected error of the test case
+    """
+    return self._error
+
+
+class RequiresAsserts(Protocol):
+  """ Protocol for objects that require 'assert*' methods usable by CaseExecutor. """
+  # pylint: disable=invalid-name
+  # noinspection PyPep8Naming
+  def assertDictEqual(
+      self,
+      first: dict[str, None | bool | int | str | set[str]],
+      second: dict[str, None | bool | int | str | set[str]]) -> None:
+    """ Compare two dictionaries for equality. """
+
+  # pylint: disable=invalid-name
+  # noinspection PyPep8Naming
+  def assertSetEqual(
+      self,
+      first: set[str],
+      second: set[str]) -> None:
+    """ Compare two sets for equality. """
+
+  # pylint: disable=invalid-name
+  # noinspection PyPep8Naming
+  def assertRaises(self, expected_exception, *args, **kwargs):
+    """ Assert that an exception is raised. """
+
+
+# pylint: disable=too-few-public-methods
+class CaseExecutor(RequiresAsserts):
+  """
+    A class to execute test cases.
+  """
+  def __init__(self,
+    function: Callable[[list[str]], tuple[dict[str, None | bool | int | str | set[str]], set[str]]]):
+    """
+      Initialize the CaseExecutor.
+    """
+    self.function = function
+
+  def execute(self, case: CaseData) -> None:
+    """
+      Execute a test case.
+
+      :param case: The test case to execute
+    """
+    if case.expected is not None:
+      expected_options, expected_parameters = case.expected
+      options, parameters = self.function(case.args)
+      self.assertDictEqual(expected_options, options)
+      self.assertSetEqual(expected_parameters, parameters)
+    else:
+      assert case.error is not None
+      with self.assertRaises(case.error):
+        self.function(case.args)
